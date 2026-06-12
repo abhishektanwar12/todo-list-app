01b575e5f085811e0de1994848f928493ff86c93
abhishektanwar12 <abhishektanwar753@gmail.com>
2026-06-12 16:35:06 +0530

    Completed To Do App

---
+++ script.js
@@ -0,0 +1,157 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const themeBtn = document.getElementById("themeBtn");
+const clearBtn = document.getElementById("clearBtn");
+const taskCounter = document.getElementById("taskCounter");
+
+loadTasks();
+
+function updateCounter(){
+    taskCounter.innerText =
+        `Tasks: ${taskList.children.length}`;
+}
+
+function saveTasks(){
+    localStorage.setItem(
+        "tasks",
+        taskList.innerHTML
+    );
+}
+
+function addTask(){
+
+    const taskText = taskInput.value.trim();
+
+    if(taskText === ""){
+        alert("Enter a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.innerText = taskText;
+
+    span.addEventListener("click", function(){
+
+        span.classList.toggle("completed");
+
+        saveTasks();
+    });
+
+    const deleteBtn =
+        document.createElement("button");
+
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList.add("delete-btn");
+
+    deleteBtn.addEventListener("click",
+        function(){
+
+            li.remove();
+
+            updateCounter();
+
+            saveTasks();
+        });
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+
+    updateCounter();
+
+    saveTasks();
+}
+
+addBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress",
+    function(e){
+
+        if(e.key === "Enter"){
+            addTask();
+        }
+});
+
+themeBtn.addEventListener("click",
+    function(){
+
+        document.body.classList
+            .toggle("dark-mode");
+
+        if(document.body.classList
+            .contains("dark-mode")){
+
+            themeBtn.innerText =
+                "☀️ Light Mode";
+        }
+        else{
+
+            themeBtn.innerText =
+                "🌙 Dark Mode";
+        }
+});
+
+clearBtn.addEventListener("click",
+    function(){
+
+        taskList.innerHTML = "";
+
+        updateCounter();
+
+        saveTasks();
+});
+
+function loadTasks(){
+
+    const savedTasks =
+        localStorage.getItem("tasks");
+
+    if(savedTasks){
+
+        taskList.innerHTML =
+            savedTasks;
+
+        reattachEvents();
+    }
+
+    updateCounter();
+}
+
+function reattachEvents(){
+
+    document.querySelectorAll("li")
+    .forEach(function(li){
+
+        const span =
+            li.querySelector("span");
+
+        const deleteBtn =
+            li.querySelector("button");
+
+        span.addEventListener("click",
+            function(){
+
+                span.classList
+                    .toggle("completed");
+
+                saveTasks();
+        });
+
+        deleteBtn.addEventListener(
+            "click",
+            function(){
+
+                li.remove();
+
+                updateCounter();
+
+                saveTasks();
+        });
+    });
+}
